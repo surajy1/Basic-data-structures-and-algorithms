0f449a98f82b2dfd902b81aef92041877f37a36f
Suraj Yadav <76870806+surajy1@users.noreply.github.com>
2022-11-05 23:38:10 +0530

    Update Print all Divisors of a number.cpp

--- Mathematics/Print all Divisors of a number.cpp
+++ Mathematics/Print all Divisors of a number.cpp
@@ -7,11 +7,13 @@ using namespace std;
 void printDivisors(int n) {
 
     int i;
-
+    
+    // Print all divisors from 1 (inclusive) to sqrt(n) (exclusive)
     for (i = 1; i * i < n; i++)
         if (n % i == 0)
             cout << i << " ";
-
+    
+    // Print all divisors from sqrt(n) (inclusive) to n (inclusive)
     for (; i >= 1; i++)
         if (n % i == 0)
             cout << i << " ";
