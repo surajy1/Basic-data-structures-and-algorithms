5af92c8477228414bc4d23141f7318cf4a30f3cf
Suraj Yadav <76870806+surajy1@users.noreply.github.com>
2022-12-08 23:39:46 +0530

    Create Check if Kth bit is set or not (using right shift).cpp

---
+++ Bit Magic/Check if Kth bit is set or not (using right shift).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/****************************/
+// Check if Kth bit is set or not (using right shift >>)
+
+bool isKthBitSet(int n, int k) {
+	if ((n >> (k - 1)) & 1)
+		return true;
+	else
+		return false;
+}
+
+/***************************/
+
+int main() {
+
+	int n, k;
+
+	cin >> n >> k;
+
+	isKthBitSet(n, k);
+
+	return 0;
+}
