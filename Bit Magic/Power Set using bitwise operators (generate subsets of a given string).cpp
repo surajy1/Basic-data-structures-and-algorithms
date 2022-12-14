3f23f15b9a75476451647752aeb4afebc0e8e8f6
Suraj Yadav <76870806+surajy1@users.noreply.github.com>
2022-12-14 19:41:03 +0530

    Create Power Set using bitwise operators (generate subsets of a given string).cpp

---
+++ Bit Magic/Power Set using bitwise operators (generate subsets of a given string).cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/****************************/
+/*
+	Power Set using bitwise operators
+	(generate subsets of a given string)
+
+	Ex. 1:
+	IP: "abc"
+	OP: "", "a", "b", "c", "ab", "bc", "ac", "abc"
+*/
+
+void printPowerSet(string str) {
+	int n = str.length();
+	int powSize = pow(2, n);
+
+	for (int counter = 0; counter < powSize; counter++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if ((counter & (1 << j)) != 0)
+			{
+				cout << str[j];
+			}
+		}
+		cout << "\n";
+	}
+}
+
+// TC: Theta(pow(2, n) * str.length())
+// SC: Theta(pow(2, n))
+/***************************/
+
+int main() {
+
+	string str;
+
+	cin >> str;
+
+	printPowerSet(str);
+
+	return 0;
+}
