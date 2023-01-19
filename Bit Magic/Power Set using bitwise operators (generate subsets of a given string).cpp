ecdb0405ee1a292084c150d2ed52086922ca97e9
Suraj Yadav <76870806+surajy1@users.noreply.github.com>
2023-01-19 18:14:01 +0530

    Update Power Set using bitwise operators (generate subsets of a given string).cpp

--- Bit Magic/Power Set using bitwise operators (generate subsets of a given string).cpp
+++ Bit Magic/Power Set using bitwise operators (generate subsets of a given string).cpp
@@ -20,12 +20,9 @@ void printPowerSet(string str) {
 	for (int counter = 0; counter < powSize; counter++)
 	{
 		for (int j = 0; j < n; j++)
-		{
 			if ((counter & (1 << j)) != 0)
-			{
 				cout << str[j];
-			}
-		}
+
 		cout << "\n";
 	}
 }
