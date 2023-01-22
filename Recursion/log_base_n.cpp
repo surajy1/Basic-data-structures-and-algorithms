dfe6c191e318fc57d960d24873d3a91e45c009b2
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-01-22 22:07:08 +0530

    Create log_base_n.cpp

---
+++ Recursion/log_base_n.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function to return log of a number
+int logbase2(int n) {
+	if (n == 1) // for logbase3 use `if (n > 3)`
+		return 0;
+
+	else
+		return 1 + logbase2(n / 2); // for logbase3 use `return logbase3(n / 3);`
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	cout << logbase2(n);
+
+	return 0;
+}
