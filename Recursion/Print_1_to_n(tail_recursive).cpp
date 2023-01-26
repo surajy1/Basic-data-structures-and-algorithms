df8f741c85fb26de5a373f50c22f889b6ce20679
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-01-26 20:25:36 +0530

    Create Print_1_to_n(tail_recursive).cpp

---
+++ Recursion/Print_1_to_n(tail_recursive).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Tail recursive function to print 1 to N
+
+void print1ToN(int n, int k) {
+	if (n == 0)
+		return;
+	cout << k << " ";
+	print1ToN(n - 1, k + 1);
+}
+// TC: O(n)
+// SC: O(n)
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	print1ToN(n, 1);
+
+	return 0;
+}
