fc61954fd909e885ead3d54edaaa219b0d858e94
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-01-26 20:29:35 +0530

    Create Factorial_of_a_number_(tail_recursion).cpp

---
+++ Recursion/Factorial_of_a_number_(tail_recursion).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Tail recursive function for factorial
+
+int fact(int n, int k) {
+	if (n == 0 || n == 1)
+		return k;
+
+	return fact(n - 1, k * n);
+}
+
+// TC: O(n)
+// SC: O(n)
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	cout << fact(n, 1);
+
+	return 0;
+}
