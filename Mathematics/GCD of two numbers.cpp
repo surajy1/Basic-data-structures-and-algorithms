e5353d0c6bbbf40ba8d15d4af84ac4cd8d90deb4
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-14 17:00:53 +0530

    Update GCD of two numbers.cpp

--- Mathematics/GCD of two numbers.cpp
+++ Mathematics/GCD of two numbers.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 /*********************/
 // GDC/HCF of two numbers
+/*
+    Input 1: a = 4, b = 6
+    Output 1: 2
+
+    Input 2: a = 100, b = 200
+    Output 2: 100
+
+    Input 3: a = 7, b = 13
+    Output 2: 1
+*/
 
 int gcd(int a, int b){
 
