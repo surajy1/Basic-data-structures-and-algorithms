7884196fea53bb259b1f1b34d742ef471f51eacd
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-14 17:08:09 +0530

    Update LCM of two numbers (recursive solution).cpp

--- Mathematics/LCM of two numbers (recursive solution).cpp
+++ Mathematics/LCM of two numbers (recursive solution).cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 /*********************/
 // LCM of two numbers
+/*
+    Input 1: a = 4, b = 6
+    Output 1: 12
+
+    Input 2: a = 12, b = 15
+    Output 2: 60
+
+    Input 3: a = 2, b = 8
+    Output 2: 8
+*/
 
 int gcd(int a, int b){
 
