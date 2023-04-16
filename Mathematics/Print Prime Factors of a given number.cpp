eff4658c23009ac02372d7327d6c13ed8eec90e6
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-16 12:27:29 +0530

    Update Print Prime Factors of a given number.cpp

--- Mathematics/Print Prime Factors of a given number.cpp
+++ Mathematics/Print Prime Factors of a given number.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 /*************************************/
 // Print prime factors of a given number
+
+/*
+    Input1: 84
+    Output1: 2 2 3 7
+    
+    Input2: 13
+    Output2: 13
+    
+    Input3: 450
+    Output3: 2 3 3 5 5
+    
+    Input4: 52
+    Output4: 2 2 13
+    
+    Input5: 144
+    Output5: 2 2 2 2 3 3
+*/
 void printPrimeFactors(int n) {
 
     if (n <= 1) return;
@@ -11,29 +28,29 @@ void printPrimeFactors(int n) {
     {
         cout << 2 << " ";
 
-        n /= 2;
+        n = n / 2;
     }
 
     while (n % 3 == 0)
     {
         cout << 3 << " ";
 
-        n /= 3;
+        n = n / 3;
     }
 
-    for (int i = 5; i * i <= n; i += 6)
+    for (int i = 5; i * i <= n; i = i + 6)
     {
         while (n % i == 0)
         {
             cout << i << " ";
 
-            n /= i;
+            n = n / i;
         }
 
         while (n % (i + 2) == 0)
         {
             cout << i + 2 << " ";
-            n /= (i + 2);
+            n = n / (i + 2);
         }
     }
 
