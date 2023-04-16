16d0a90a69e2fdfc94c365ce579cc66dbd7be581
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-16 12:49:55 +0530

    Update Print all Prime numbers that are <= n (Sieve of Eratosthenes).cpp

--- Mathematics/Print all Prime numbers that are <= n (Sieve of Eratosthenes).cpp
+++ Mathematics/Print all Prime numbers that are <= n (Sieve of Eratosthenes).cpp
@@ -4,6 +4,28 @@ using namespace std;
 /***********************************************/
 // Sieve of Eratosthenes
 // Print all Prime numbers that are <= n
+/*
+    Input1: 10
+    Output1: 2 3 5 7
+    
+    Input2: 23
+    Output2: 2 3 5 7 11 13 17 19 23
+    
+    Input3: 50
+    Output3: 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+    
+    Input4: 83
+    Output4: 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83
+    
+    Input5: 2
+    Output5: 2
+    
+    Input6: 7
+    Output6: 2 3 5 7
+    
+    Input7: 19
+    Output7: 2 3 5 7 11 13 17 19
+*/
 
 void sieve(int n)
 {
@@ -15,7 +37,7 @@ void sieve(int n)
         {
             cout << i << " ";
 
-            for (int j = i * i; j <= n; j += i)
+            for (int j = i * i; j <= n; j = j + i)
                 isPrime[j] = false;
         }
     }
