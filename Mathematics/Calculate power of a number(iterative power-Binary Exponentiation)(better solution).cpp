b4802476c48860220e19f1aa4df6aea3c17648bc
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-17 18:43:28 +0530

    Update Calculate power of a number(iterative power-Binary Exponentiation)(better solution).cpp

--- Mathematics/Calculate power of a number(iterative power-Binary Exponentiation)(better solution).cpp
+++ Mathematics/Calculate power of a number(iterative power-Binary Exponentiation)(better solution).cpp
@@ -5,22 +5,39 @@ using namespace std;
 /* 
 	Power of a number using iterative power
 	(Binary Exponentiation) 
+	
+	Base1: 5
+	Exp1: 2
+	Ans: 25
+
+	Base2: 2
+	Exp2: 5
+	Ans: 32
+
+	Base3: 2
+	Exp3: -1
+	Ans: 0.5
+	
+	More test cases after main()
 */
 
-int power(int base, int exp) {
+double power(double base, int exp) {
 
-	int res = 1;
+	double res = 1.0;
+	int n = abs(exp);
 
-	while (exp > 0)
+	while (n > 0)
 	{
-		if (exp & 1) //equivalent to if(exp % 2 != 0)
+		if (n & 1) //equivalent to if(n % 2 != 0)
 			res = res * base;
 
 		base = base * base;
-		exp = exp >> 1; //equivalent to exp = exp / 2;
+		n = n >> 1; //equivalent to n = n / 2;
 	}
+	if (exp > 0)
+		return res;
 
-	return res;
+	return double(1.0) / double(res);
 }
 
 // TC: O(log(n))
@@ -30,7 +47,7 @@ int power(int base, int exp) {
 int main() {
 
 
-	int base, exp;
+	double base, exp;
 
 	cin >> base >> exp;
 
@@ -38,3 +55,25 @@ int main() {
 
 	return 0;
 }
+
+/*
+	Base4: 2
+	Exp4: 2
+	Ans: 4
+
+	Base5: 9
+	Exp5: 3
+	Ans: 729
+
+	Base6: 12
+	Exp6: -2
+	Ans: 0.00694444
+
+	Base7: 5
+	Exp7: -3
+	Ans: 0.008
+
+	Base8: 2
+	Exp8: 10
+	Ans: 1024
+*/
