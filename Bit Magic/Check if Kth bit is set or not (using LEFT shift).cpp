31ccb4871b77c105fd249c8156b5994cca79d47b
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-19 20:13:22 +0530

    Update Check if Kth bit is set or not (using LEFT shift).cpp

--- Bit Magic/Check if Kth bit is set or not (using LEFT shift).cpp
+++ Bit Magic/Check if Kth bit is set or not (using LEFT shift).cpp
@@ -3,12 +3,46 @@ using namespace std;
 
 /****************************/
 // Check if Kth bit is set or not (using left shift <<)
+/*
+	Input1: 5 4
+	Output1: 0
+
+	Input2: 5 3
+	Output2: 1
+
+	Input3: 5 2
+	Output3: 0
+
+	Input4: 5 1
+	Output4: 1
+
+	Input5: 8 4
+	Output5: 1
+
+	Input6: 8 3
+	Output6: 0
+
+	Input7: 10 5
+	Output7: 0
+
+	Input8: 10 4
+	Output8: 1
+
+	Input9: 10 2
+	Output9: 1
+
+	Input10: 11 1
+	Output10: 1
+*/
 
 bool isKthBitSet(int n, int k) {
 	if (n & (1 << (k - 1)))
 		return true;
 	else
 		return false;
+	
+	// ALTRNATIVE
+	// return ((n & (1 << (k - 1))) > 0);
 }
 
 /***************************/
