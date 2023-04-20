968ba10109be1af87c79de9e3d340ce0685903a6
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-20 13:44:57 +0530

    Update Count set bits (Brian Kerningam's Algorithm).cpp

--- Bit Magic/Count set bits (Brian Kerningam's Algorithm).cpp
+++ Bit Magic/Count set bits (Brian Kerningam's Algorithm).cpp
@@ -4,7 +4,37 @@ using namespace std;
 /****************************/
 // Count set bits
 // (Brian Kerningam's Algorithm)
+/*
+	Input1: 5
+	Output1: 2
 
+	Input2: 10
+	Output2: 2
+
+	Input3: 11
+	Output3: 3
+
+	Input4: 31
+	Output4: 5
+
+	Input5: 21
+	Output5: 3
+
+	Input6: 32
+	Output6: 1
+
+	Input7: 1000
+	Output7: 6
+
+	Input8: 1024
+	Output8: 1
+
+	Input9: 2000
+	Output9: 6
+
+	Input10: 13
+	Output10: 3
+*/
 int countSetBits(int n) {
 	int res = 0;
 
