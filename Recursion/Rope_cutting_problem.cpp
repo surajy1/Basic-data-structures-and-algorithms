d4363fd94896a673f657c08b2626f0fbb99a8653
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-24 14:14:27 +0530

    Update Rope_cutting_problem.cpp
    Adding problem statements and sample test cases.

--- Recursion/Rope_cutting_problem.cpp
+++ Recursion/Rope_cutting_problem.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+/*
+	Given a rope of length n, 
+	you need to find the maximum number of pieces
+	you can make such that the length of every piece 
+	is in set {a, b, c} for the given three values a, b, c.
 
+	Input1: n=5, a=2, b=5, c=1
+	Output1: 5  (5 pieces each of length 1)
+
+	Input2: n=23, a=12, b=9, c=11
+	Output2: 2 (a + b = 12 + 11 = 23)
+
+	Input3: n=5, a=4, b=2, c=6
+	Output3: -1 (Not possible)
+*/
 int maxPieces(int n, int a, int b, int c) {
 	if (n == 0) return 0;
 	if (n < 0) return -1;
