5971a18de104b42d7296c101d1815266f6099dfa
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-05-02 18:22:49 +0530

    Update Insertion sort.cpp

--- Sorting/Insertion sort.cpp
+++ Sorting/Insertion sort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // Output: 3 4 5 6 9
 
 void insertion_sort(int arr[], int n) {
-	for (int i = 0; i < n - 1; i++) {
+	for (int i = 0; i <= n - 1; i++) {
 		int j = i;
 		while (j > 0 && arr[j - 1] > arr[j]) {
 			swap(arr[j - 1], arr[j]);
