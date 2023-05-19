e0688df15fdf643ac8cedad28fab3dc69acf5844
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-05-19 20:01:41 +0530

    Create Print second largest element.cpp

---
+++ Array/tuf/Print second largest element.cpp
@@ -0,0 +1,49 @@
+// https://practice.geeksforgeeks.org/problems/second-largest3735/1
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    int largest = -1;
+	    int secLargest = -1;
+	    for(int i=0; i<n; i++) {
+	        if(arr[i] > largest) {
+	            secLargest = largest;
+	            largest = arr[i];
+	        }
+	        else if(arr[i] > secLargest && arr[i] != largest) {
+	            secLargest = arr[i];
+	        }
+	    }
+	    return secLargest;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
